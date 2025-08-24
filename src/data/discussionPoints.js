b7c1c3e0d005130d81403d39a0166889c967aac2
Austin <austinnabilblass@gmail.com>
2025-08-24 13:24:49 -0400

    Update Discussion Points for Grammer

--- src/data/discussionPoints.js
+++ src/data/discussionPoints.js
@@ -1,13 +1,6 @@
 const discussionPoints = [
   {
     id: 1,
-    title: "Congressional App Challenge (2025)",
-    content: `Developed Local Lens Reporting, a web application to help fight news desserts by bringing 
-            journalists directly to stories in their regions, for the Congressional App Challlenge.`,
-    image: "image1.jpg"
-  },
-  {
-    id: 2,
     title: "Worked in Open Source",
     content: `Worked on open source software development through rendering and physics software, database engines,
             autonomous trading, business tools such as ERP and CRM, and more. (Learn more at my 
@@ -15,21 +8,28 @@ const discussionPoints = [
     image: "image2.jpg"
   },
   {
-    id: 3,
+    id: 2,
     title: "Professional Experience",
-    content: `Developed and implemented a digital time tracking system for The Norwood School, replacing manual 
+    content: `Developed and implemented a digital time tracking system for Norwood School, replacing manual 
             Excel-based processes with an automated web solution. This streamlined employee time management, reduced 
             administrative overhead, and improved data accuracy.`,
     image: "image3.jpg"
   },
   {
-    id: 4,
+    id: 3,
     title: "Working on the Skill Bytes Project",
     content: `Working on developing Skill Bytes, an application to help developers learn new skills and languages,
             prevent deterioration of skills from AI, and collaborate with other developers. (Check out the 
             <a href="https://skill-bytes.netlify.app" target="_blank" rel="noopener noreferrer" style="color: #4da6ff; text-decoration: none;">Skill Bytes</a> website. The project is still in development, so certain features may not be 
             completed, however I'm aiming for all core features to be completed by the end of 2025.)`,
     image: "image4.jpg"
+  },
+  {
+    id: 4,
+    title: "Congressional App Challenge (2025)",
+    content: `Developed Local Lens Reporting, a web application to help fight news desserts by bringing 
+            journalists directly to stories in their regions, for the Congressional App Challlenge.`,
+    image: "image1.jpg"
   }
 ];
 
